5f61b60e050a8a878edad89306bb1da9c59ca872
Ccaarrrllll <ccaarrrllll@proton.me>
2026-09-13 13:30:06 -0400

    pipeline changes

--- Gemfile
+++ Gemfile
@@ -1,7 +1,9 @@
 source 'https://rubygems.org'
 
+gem 'jekyll'
+gem 'jekyll-sass-converter', '~> 3.0'
+
 group :jekyll_plugins do
-    gem 'github-pages'
     gem 'jekyll-feed'
     gem 'jekyll-gist'
     gem 'jekyll-seo-tag'
--- _plugins/ruby33_compat.rb
+++
@@ -1,9 +0,0 @@
-# Ruby 3.3 compatibility fix
-# Liquid 4.0.3 uses String#tainted? which was removed in Ruby 3.2
-class String
-  unless instance_methods.include?(:tainted?)
-    def tainted?
-      false
-    end
-  end
-end
